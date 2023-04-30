e8c08963abf6ebe5d2e7a6d55d217cb35393c7b1
Ansh Vashisht <anshvashihst99@gmail.com>
2023-04-30 23:33:43 +0530

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <Stack gap="20px" alignItems="center" px="10px" pt="24px">
         <img src={Logo} alt="logo" />
         <Typography variant="h6"x  >
-        Made with ❤️ by Satish Panchal
+        Made with ❤️ by Ansh Vashisht
         </Typography>
         <Box mb="20px" style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}} width="240px">
-          <a style={{color:"black"}} href="http://www.github.com/satisshhhhhh" target="_blank"><GitHub fontSize="large"/></a>
-          <a style={{color:"blue"}} href="https://www.linkedin.com/in/satish-panchal-26221b203/" target="_blank"><LinkedIn fontSize="large" /></a>
-          <a style={{color:"purple"}} href="https://www.instagram.com/satisshhhhhh/" target="_blank"><Instagram fontSize="large" /></a>
+          <a style={{color:"black"}} href="https://github.com/Anshv99" target="_blank"><GitHub fontSize="large"/></a>
+          <a style={{color:"blue"}} href="https://www.linkedin.com/in/ansh-vashisht-4b4b3024b/" target="_blank"><LinkedIn fontSize="large" /></a>
+          <a style={{color:"purple"}} href="https://www.instagram.com/19.ansh.v/" target="_blank"><Instagram fontSize="large" /></a>
         </Box>
       </Stack>
     </Box>
